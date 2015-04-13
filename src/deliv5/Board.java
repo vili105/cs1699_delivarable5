6c838e42a6afa28dd8e3a2068e0aa601680901f3
vili105 <velesta.stoyanova@gmail.com>
2015-04-13 19:53:14 -0400

    Implementation of drawInitialBoard()

--- src/deliv5/Board.java
+++ src/deliv5/Board.java
@@ -1,5 +1,32 @@
 package deliv5;
 
 public class Board {
+	
+	private char[][] positions = new char[3][3];
+	
+	// My first drawInitialBoard_test() fails because the class that it tests and the constructor
+	// do not exist. So, I write one that satisfies the test.
+	public Board()
+	{
+		for(int i=0; i<3; i++)
+		{
+			for(int j=0; j<3; j++)
+			{	
+				positions[i][j] = ' ';
+			}
+			
+		}
+	}
+	
+	
+	// My drawInitialBoard_test() fails because there is no implementation of the method it tests.
+	// So, I write the implementation of the method.
+	public String drawInitialBoard()
+	{
+		String temp = " | | \n------\n | | \n------\n | | \n";
+		System.out.println(temp);	
+		return temp;
+	}
+	
 
 }
