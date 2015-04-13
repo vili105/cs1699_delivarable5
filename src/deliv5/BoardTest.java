ba90c44599ea65c5bf959bc31619fe797c2f9df0
vili105 <velesta.stoyanova@gmail.com>
2015-04-13 18:50:21 -0400

    Added first test

--- src/deliv5/BoardTest.java
+++ src/deliv5/BoardTest.java
@@ -5,10 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class BoardTest {
+	
 
+	// I start with testing the initial print of the game board
+	// I decide how I want it to look and make test for it
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void drawInitialBoard_test()
+	{
+		
+		Board b = new Board();
+		assertEquals(b.drawInitialBoard()," | | \n------\n | | \n------\n | | \n");
 	}
 
 }
