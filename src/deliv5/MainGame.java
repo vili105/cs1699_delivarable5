d02c2254a6f7bbbe22888b35526753b29b5560b4
vili105 <velesta.stoyanova@gmail.com>
2015-04-14 12:23:42 -0400

    Creating Input_test() and then implementing the function Input()

--- src/deliv5/MainGame.java
+++ src/deliv5/MainGame.java
@@ -1,7 +1,10 @@
 package deliv5;
 
+import java.util.Scanner;
+
 public class MainGame {
 
+	// Function that tests if the input is valid
 	public Boolean validInput(int x, int y)
 	{
 		if((x>-1)&&(x<3)&&(y>-1)&&(y<3))
@@ -12,6 +15,19 @@ public class MainGame {
 		{
 			return false;
 		}
+	}
+	
+	
+	// Implementation of handling the player's input
+	public int[] Input()
+	{
+		int[] p_input = new int[2];
+		Scanner in = new Scanner(System.in);
+		p_input[0] = in.nextInt();
+		p_input[1] = in.nextInt();
+		in.close();
+		return p_input;
+		
 	}
 	
 
--- src/deliv5/MainGameTest.java
+++ src/deliv5/MainGameTest.java
@@ -3,11 +3,12 @@ package deliv5;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
-
+import java.io.ByteArrayInputStream;
 
 public class MainGameTest {
 	
 	MainGame g;
+	ByteArrayInputStream in;
 	
 	@Before
 	public void init()
@@ -30,4 +31,17 @@ public class MainGameTest {
 		assertFalse(g.validInput(3,5));
 	}
 
+	// Test a function that handles the player's imput
+	@Test
+	public void Input_test()
+	{
+		int[] temp = new int[2];
+		byte[] numbers = "1\n2\n".getBytes();
+		in = new ByteArrayInputStream(numbers);
+		System.setIn(in);
+		temp = g.Input();
+		assertEquals(temp[0],1);
+		assertEquals(temp[1],2);
+		
+	}
 }
