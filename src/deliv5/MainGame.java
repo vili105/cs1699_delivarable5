8e7cf078500faa5269837f62abc5fff030e9d30b
vili105 <velesta.stoyanova@gmail.com>
2015-04-14 01:27:35 -0400

    Created validIntut_test1() and implemented validIntut(), such that both test will pass.

--- src/deliv5/MainGame.java
+++ src/deliv5/MainGame.java
@@ -4,7 +4,14 @@ public class MainGame {
 
 	public Boolean validInput(int x, int y)
 	{
-		return true;
+		if((x>-1)&&(x<3)&&(y>-1)&&(y<3))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 	
 
--- src/deliv5/MainGameTest.java
+++ src/deliv5/MainGameTest.java
@@ -1,17 +1,33 @@
 package deliv5;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class MainGameTest {
+	
+	MainGame g;
+	
+	@Before
+	public void init()
+	{
+		g = new MainGame();
+	}
+	
 
 	// Tests a function that checks if the input is valid
 	@Test
 	public void validInput_test()
 	{	
-		MainGame g = new MainGame();
 		assertTrue(g.validInput(1,1));
+	}
+	
+	// Tests a function that checks if the input is valid
+	@Test
+	public void validInput_test1()
+	{	
+		assertFalse(g.validInput(3,5));
 	}
 
 }
